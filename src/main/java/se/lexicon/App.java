be61165b329c2db5abd78470a285dfd1c89294b7
Simon Elbrink <simon.elbrink@gmail.com>
2022-02-18 09:36:36 +0100

    .

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,5 +1,12 @@
 package se.lexicon;
 
+import se.lexicon.dao.*;
+import se.lexicon.dao.sequencer.SequencersImpl;
+import se.lexicon.model.entity.Student;
+import se.lexicon.service.LearnedSkillService;
+import se.lexicon.service.LearnedSkillServiceImpl;
+import se.lexicon.service.SkillServiceImpl;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        StudentDAO studentDAO = new StudentDAOImpl(new SequencersImpl());
+
+        studentDAO.save(new Student("Tobias"));
+
+        studentDAO.findAll().forEach(System.out::println);
+
+        LearnedSkillService skillService = new LearnedSkillServiceImpl(new LearnedSkillDAOImpl(new SequencersImpl()),
+                new SkillServiceImpl(new SkillDAOImpl(new SequencersImpl())));
+
+        skillService.findAll();
+
+
     }
 }
